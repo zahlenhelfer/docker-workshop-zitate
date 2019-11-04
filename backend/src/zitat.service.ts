6fa8ca1304afc5293b306ec69d44325b06b66603
Peter Dickten <peter.dickten@dcs-fuerth.de>
2019-11-04 19:59:59 +0100

    initial

---
+++ backend/src/zitat.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+const lineReader = require('line-reader');
+import * as path from 'path';
+
+@Injectable()
+export class ZitatService {
+  zitate = [];
+
+  constructor() {
+    this.readFile();
+  }
+
+  getPathToFile(fileName: string): string {
+    return path.resolve(__dirname, fileName);
+  }
+
+  readFile() {
+    lineReader.eachLine(this.getPathToFile('../data/zitate.txt'), line => {
+      this.zitate.push(line);
+    });
+  }
+
+  getZitat(): object {
+    const index = Math.round(Math.random() * (this.zitate.length - 1));
+    return { zitat: this.zitate[index] };
+  }
+}
